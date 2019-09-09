13b54c4fb832172a24375f77876f0c9ad19e0099
April <aprilchi4@gmail.com>
2019-09-10 02:29:24 +1000

    bullets get removed

--- asteroids.js
+++ asteroids.js
@@ -3,76 +3,34 @@ function asteroids() {
     const svg = document.getElementById("canvas");
     let gameTimer = 150, gameComplete = false;
     const gameObservable = Observable.interval(gameTimer).filter(() => !gameComplete);
+    const asteroidObservable = Observable.interval(1);
     const keydown$ = Observable.fromEvent(document, 'keydown');
     const keyup$ = Observable.fromEvent(document, 'keyup');
     let g = new Elem(svg, 'g')
         .attr("transform", "translate(300 300) rotate(300)");
     let shipMove = /translate\((\d+) (\d+)\) rotate\((\d+)\)/.exec(g.attr("transform"));
-    let ship = new Elem(svg, 'polygon', g.elem)
-        .attr("points", "-15,20 15,20 0,-30")
-        .attr("style", "fill:#f4e46c;stroke:#f4e46c;stroke-width:1");
-    let asteroid = new Elem(svg, "circle")
-        .attr("style", "fill:#9bd5bd;stroke:#9bd5bd;stroke-width:2")
-        .attr("cx", 300)
-        .attr("cy", 20)
-        .attr("r", 50);
     let translateX = Number(shipMove[1]);
     let translateY = Number(shipMove[2]);
     let rotation = Number(shipMove[3]);
+    let ship = new Elem(svg, 'polygon', g.elem)
+        .attr("points", "-15,20 15,20 0,-30")
+        .attr("style", "fill:#f4e46c;stroke:#f4e46c;stroke-width:1");
+    let asteroidArray = [];
+    asteroidObservable
+        .takeUntil(asteroidObservable.filter(i => i == 5))
+        .subscribe((e) => {
+        let asteroidRandomX = getRandomInt(0, 250);
+        let asteroidRandomY = getRandomInt(0, 400);
+        let asteroid = new Elem(svg, "circle")
+            .attr("style", "fill:#9bd5bd;stroke:#9bd5bd;stroke-width:2")
+            .attr("cx", asteroidRandomX)
+            .attr("cy", asteroidRandomY)
+            .attr("r", 50);
+        asteroidArray.push(asteroid);
+    });
     function getRandomInt(min, max) {
         return Math.floor((Math.random() + min) * Math.floor(max));
     }
-    function getDirection() {
-        return getRandomInt(0, 2) === 0 ? -1 : 1;
-    }
-    let directionX = getDirection();
-    let directionY = getDirection();
-    let velocityX = getRandomInt(1, 8);
-    let velocityY = getRandomInt(1, 8);
-    gameObservable.subscribe(() => {
-        asteroid.attr("cx", directionX * velocityX + Number(asteroid.attr("cx")));
-        asteroid.attr("cy", directionY * velocityY + Number(asteroid.attr("cy")));
-    });
-    gameObservable
-        .map(() => ({
-        asteroidX: Number(asteroid.attr("cx")),
-        asteroidY: Number(asteroid.attr("cy")),
-        asteroidR: Number(asteroid.attr("r"))
-    }))
-        .filter(({ asteroidX }) => ((asteroidX > 600 + 10)))
-        .subscribe(() => {
-        asteroid.attr("cx", -10);
-    });
-    gameObservable
-        .map(() => ({
-        asteroidX: Number(asteroid.attr("cx")),
-        asteroidY: Number(asteroid.attr("cy")),
-        asteroidR: Number(asteroid.attr("r"))
-    }))
-        .filter(({ asteroidX }) => ((asteroidX < -10)))
-        .subscribe(() => {
-        asteroid.attr("cx", 600 + 10);
-    });
-    gameObservable
-        .map(() => ({
-        asteroidX: Number(asteroid.attr("cx")),
-        asteroidY: Number(asteroid.attr("cy")),
-        asteroidR: Number(asteroid.attr("r"))
-    }))
-        .filter(({ asteroidY }) => ((asteroidY > 600 + 10)))
-        .subscribe(() => {
-        asteroid.attr("cy", -10);
-    });
-    gameObservable
-        .map(() => ({
-        asteroidX: Number(asteroid.attr("cx")),
-        asteroidY: Number(asteroid.attr("cy")),
-        asteroidR: Number(asteroid.attr("r"))
-    }))
-        .filter(({ asteroidX }) => ((asteroidX < -10)))
-        .subscribe(() => {
-        asteroid.attr("cy", 600 + 10);
-    });
     keydown$
         .map(({ key }) => {
         return key;
@@ -114,7 +72,7 @@ function asteroids() {
             .attr("style", "fill:#ffffff;stroke:#ffffff;stroke-width:2")
             .attr("cx", translateX)
             .attr("cy", translateY)
-            .attr("r", 5);
+            .attr("r", 4);
         bulletsArray.push(bulletShot);
         let currentState = Observable.interval(100).map(x => ({ x, currBullet: bulletShot }));
         currentState
@@ -142,23 +100,14 @@ function asteroids() {
         bulletArray: bulletsArray
     })).map((e) => {
         return e.bulletArray.filter((bulletArray) => (Number(bulletArray.attr("cx")) <= 600 && (Number(bulletArray.attr("cy")) <= 600) && (Number(bulletArray.attr("cy")) >= 0) && Number(bulletArray.attr("cx")) >= 0));
-    }).subscribe(array => {
-        bulletsArray = array;
-    });
+    }).subscribe(array => (bulletsArray = array));
     gameObservable.map(x => ({
         x,
-        currAsteroid: asteroid,
-        bulletArray: bulletsArray
-    })).map((e) => {
-        let asteroidXCircle = Number(e.currAsteroid.attr("cx"));
-        let asteroidYCircle = Number(e.currAsteroid.attr("cy"));
-        let asteroidRadius = Number(e.currAsteroid.attr("r"));
-        return e.bulletArray.filter((bullet) => (checkCollision(Number(bullet.attr("cx")), asteroidXCircle, Number(bullet.attr("cy")), asteroidYCircle, asteroidRadius, Number(bullet.attr("r")))));
-    })
-        .subscribe((returnArray) => {
-        returnArray.forEach((element) => element.elem.remove());
-        console.log(returnArray);
-    });
+        myBulletArray: bulletsArray,
+        myAsteroidArray: asteroidArray
+    })).map(({ x, myBulletArray, myAsteroidArray }) => (myAsteroidArray.filter((asteroid) => (myBulletArray.filter((bullet) => (checkCollision(Number(bullet.attr("cx")), Number(asteroid.attr("cx")), Number(bullet.attr("cy")), Number(asteroid.attr("cy")), Number(asteroid.attr("r")), Number(bullet.attr("r")))))
+        .map(bullet => (bullet.elem.remove()))))))
+        .subscribe((returnArray) => (console.log(returnArray)));
 }
 if (typeof window != 'undefined')
     window.onload = () => {
--- asteroids.ts
+++ asteroids.ts
@@ -15,14 +15,16 @@ function asteroids() {
 
   const svg = document.getElementById("canvas")!;
 
-  let   gameTimer      = 150,
-        gameComplete   = false
-  const gameObservable = Observable.interval(gameTimer).filter(() => !gameComplete)
+  let   gameTimer          = 150,
+        gameComplete       = false
+  const gameObservable     = Observable.interval(gameTimer).filter(() => !gameComplete)
+  const asteroidObservable = Observable.interval(1)                                      // create the asteroids
 
   // creates new observable that emits an event object everytime a keydown is fired
   // emits keyboard event everytime user presses down a key
   const keydown$ = Observable.fromEvent<KeyboardEvent>(document, 'keydown');
   const keyup$   = Observable.fromEvent<KeyboardEvent>(document, 'keyup');
+
 
   // make a group for the spaceship and a transform to move it and rotate it
   // to animate the spaceship you will update the transform property
@@ -36,106 +38,128 @@ function asteroids() {
   // shipMove[2] = rotation
   let shipMove = /translate\((\d+) (\d+)\) rotate\((\d+)\)/.exec(g.attr("transform"))!;
 
+  // keep track of ship coordinates for ship movement(translate x, translate y, rotation)
+  let translateX = Number(shipMove[1])
+  let translateY = Number(shipMove[2])
+  let rotation   = Number(shipMove[3])
+  
+
   // create ship svg
   let ship = new Elem(svg, 'polygon', g.elem)
     .attr("points", "-15,20 15,20 0,-30")
     .attr("style", "fill:#f4e46c;stroke:#f4e46c;stroke-width:1")
 
-  // create asteriod svg
-  let asteroid = new Elem(svg, "circle")
-    .attr("style", "fill:#9bd5bd;stroke:#9bd5bd;stroke-width:2")
-    .attr("cx", 300) // follow where the arrow is
-    .attr("cy", 20)
-    .attr("r", 50)
+  /*  Logic to create asteroids that move randomly -> put them into an array so we can keep track of them */
+  let asteroidArray: Elem[] = []  // impure
 
-  // keep track of ship coordinates (translate x, translate y, rotation)
-  let translateX = Number(shipMove[1])
-  let translateY = Number(shipMove[2])
-  let rotation   = Number(shipMove[3])
+  asteroidObservable
+    .takeUntil(asteroidObservable.filter(i => i == 5)) // this part taken from Harsil's code
+    .subscribe((e) => {
+      // create random starting points
+      let asteroidRandomX = getRandomInt(0, 250)
+      let asteroidRandomY = getRandomInt(0, 400)
 
+      // create asteriod svg
+      let asteroid = new Elem(svg, "circle")
+      .attr("style", "fill:#9bd5bd;stroke:#9bd5bd;stroke-width:2")
+      .attr("cx", asteroidRandomX) // follow where the arrow is
+      .attr("cy", asteroidRandomY)
+      .attr("r", 50)
+      // console.log(asteroidRandomX)
+      // console.log(asteroidRandomY)
+
+      // push asteroid into array
+      asteroidArray.push(asteroid)
+
+      // console.log(asteroidArray)
+
+    })
+
+
+/* LOGIC FOR RANDOM ASTEROID MOVEMENT */
   // used to get a random integer for asteroid random movementts
   // inspired by: https://stackoverflow.com/questions/52015418/random-movement-angular
   function getRandomInt(min: number, max: number) {
     return Math.floor((Math.random() + min) * Math.floor(max));
   }
 
-  // Then you will need to determine the direction, if it is negative (go left) or positive (go right):
-  function getDirection() {
-    return getRandomInt(0, 2) === 0 ? -1: 1;
-  }
+  // // Then you will need to determine the direction, if it is negative (go left) or positive (go right):
+  // function getDirection() {
+  //   return getRandomInt(0, 2) === 0 ? -1: 1;
+  // }
 
-  let directionX = getDirection();
-  let directionY = getDirection();
+  // let directionX = getDirection();
+  // let directionY = getDirection();
 
-  let velocityX = getRandomInt(1, 8);  // the number will be between -8 and 8 excluding 0
-  let velocityY = getRandomInt(1, 8);  // same here
+  // let velocityX = getRandomInt(1, 8);  // the number will be between -8 and 8 excluding 0
+  // let velocityY = getRandomInt(1, 8);  // same here
 
-  // LOGIC FOR ASTEROID MOVING RANDOMLY
-  gameObservable.subscribe(() => {
-    asteroid.attr("cx", directionX * velocityX + Number(asteroid.attr("cx")))  // the ball should go towards the left or the right
-    asteroid.attr("cy", directionY * velocityY + Number(asteroid.attr("cy"))) // the ball should go up or down
-  })
+  // // LOGIC FOR ASTEROID MOVING RANDOMLY
+  // gameObservable.subscribe(() => {
+  //   asteroid.attr("cx", directionX * velocityX + Number(asteroid.attr("cx")))  // the ball should go towards the left or the right
+  //   asteroid.attr("cy", directionY * velocityY + Number(asteroid.attr("cy"))) // the ball should go up or down
+  // })
 
+  /* LOGIC FOR ASTEROID SCREEN WRAPPING */
   // LOGIC FOR ASTEROID COLLIDING WITH EDGE --> x axis
-  gameObservable
-    .map(() => ({
-      asteroidX: Number(asteroid.attr("cx")),
-      asteroidY: Number(asteroid.attr("cy")),
-      asteroidR: Number(asteroid.attr("r"))
-    }))
-    .filter(({ asteroidX }) => (
-      (asteroidX > 600 + 10)
-    ))
-    .subscribe(() => {
-      asteroid.attr("cx", -10)
-    })
+  // gameObservable
+  //   .map(() => ({
+  //     asteroidX: Number(asteroid.attr("cx")),
+  //     asteroidY: Number(asteroid.attr("cy")),
+  //     asteroidR: Number(asteroid.attr("r"))
+  //   }))
+  //   .filter(({ asteroidX }) => (
+  //     (asteroidX > 600 + 10)
+  //   ))
+  //   .subscribe(() => {
+  //     asteroid.attr("cx", -10)
+  //   })
 
 
-  // LOGIC FOR ASTEROID COLLIDING WITH EDGE --> x axis
-  gameObservable
-    .map(() => ({
-      asteroidX: Number(asteroid.attr("cx")),
-      asteroidY: Number(asteroid.attr("cy")),
-      asteroidR: Number(asteroid.attr("r"))
-    }))
-    .filter(({ asteroidX }) => (
-      (asteroidX < -10)
-    ))
-    .subscribe(() => {
-      asteroid.attr("cx", 600 + 10)
-    })
+  // // LOGIC FOR ASTEROID COLLIDING WITH EDGE --> x axis
+  // gameObservable
+  //   .map(() => ({
+  //     asteroidX: Number(asteroid.attr("cx")),
+  //     asteroidY: Number(asteroid.attr("cy")),
+  //     asteroidR: Number(asteroid.attr("r"))
+  //   }))
+  //   .filter(({ asteroidX }) => (
+  //     (asteroidX < -10)
+  //   ))
+  //   .subscribe(() => {
+  //     asteroid.attr("cx", 600 + 10)
+  //   })
 
-  // y axis screen wrap
-  gameObservable
-    .map(() => ({
-      asteroidX: Number(asteroid.attr("cx")),
-      asteroidY: Number(asteroid.attr("cy")),
-      asteroidR: Number(asteroid.attr("r"))
-    }))
-    .filter(({ asteroidY }) => (
-      (asteroidY > 600 + 10)
-    ))
-    .subscribe(() => {
-      asteroid.attr("cy", -10)
-    })
+  // // y axis screen wrap
+  // gameObservable
+  //   .map(() => ({
+  //     asteroidX: Number(asteroid.attr("cx")),
+  //     asteroidY: Number(asteroid.attr("cy")),
+  //     asteroidR: Number(asteroid.attr("r"))
+  //   }))
+  //   .filter(({ asteroidY }) => (
+  //     (asteroidY > 600 + 10)
+  //   ))
+  //   .subscribe(() => {
+  //     asteroid.attr("cy", -10)
+  //   })
 
-  // y-axis screen wrap
-  gameObservable
-    .map(() => ({
-      asteroidX: Number(asteroid.attr("cx")),
-      asteroidY: Number(asteroid.attr("cy")),
-      asteroidR: Number(asteroid.attr("r"))
-    }))
-    .filter(({ asteroidX }) => (
-      (asteroidX < -10)
-    ))
-    .subscribe(() => {
-      asteroid.attr("cy", 600 + 10)
-    })
-
-  // Logic for movement
+  // // y-axis screen wrap
+  // gameObservable
+  //   .map(() => ({
+  //     asteroidX: Number(asteroid.attr("cx")),
+  //     asteroidY: Number(asteroid.attr("cy")),
+  //     asteroidR: Number(asteroid.attr("r"))
+  //   }))
+  //   .filter(({ asteroidX }) => (
+  //     (asteroidX < -10)
+  //   ))
+  //   .subscribe(() => {
+  //     asteroid.attr("cy", 600 + 10)
+  //   })
 
 
+/* LOGIC FOR SPACESHIP MOVEMENT */
   // movement left
   keydown$ // get the repeat object and take it once itis true
     .map(({ key }) => {
@@ -174,11 +198,7 @@ function asteroids() {
       g.attr("transform", `translate(${translateX = translateX + x} ${translateY = translateY + y}) rotate(${rotation})`)
     })
 
-  // reduce observables into an array  -> takes emissions from observable and reduces them too initial value
-  // keep time and user input int oobservables and do merge etc.. combine etc... 
-  // there is merge nad merge but merge withh the latest bit 
-  // reduce user input into a game state
-
+    /* LOGIC FOR SHOOTING BULLETS AND CREATING THEM */
   // store the bullets into an array so asteroids can iterate through it
   let bulletsArray: Elem[] = [];
 
@@ -197,7 +217,7 @@ function asteroids() {
         .attr("style", "fill:#ffffff;stroke:#ffffff;stroke-width:2")
         .attr("cx", translateX) // follow where the arrow is
         .attr("cy", translateY)
-        .attr("r", 5)
+        .attr("r", 4)
 
       // add bullets into an array
       bulletsArray.push(bulletShot)
@@ -226,29 +246,17 @@ function asteroids() {
     })
 
   // LOGIC TO REMOVE ASTEROIDS OFFSCREEN FROM ARRAY
-  // ask about how to check if the bullet is within the coordinates of the circle
-  // this goes througgh the array of bullets and removes the ones that are offscreen
-  // gameObservable.map((x) => ({
-  //   timer      : x,
-  //   bulletArray: bulletsArray
-  // })).map((e) =>
-  //   e.bulletArray.filter((element) => {
-  //     (Number(element.attr("cx")) >= 600 || Number(element.attr("cy")) >= 600 || Number(element.attr("cy")) <= 0 || Number(element.attr("cx")) <= 0)
-  //   })
-  // )
-
-  // interval observable and user input oobservable. whhen they hit space, turn it intoo a bullet and subscribe to that
-  // by drawing a bullet
-  // diffrence in x^2 and difference in y^2 and if that is less than yuor radius 
-
-  function checkCollision(x1: number, x2: number, y1: number, y2: number, radius1:number, radius2: number) {
+/* Function to check if the bullet has collided with the asteroid. */
+  function checkCollision(x1: number, x2: number, y1: number, y2: number, radius1: number, radius2: number) {
     // console.log("x1: " + x1)
     // console.log("x2: " + x2)
     // console.log("y1: " + y1)
     // console.log("y2: " + y2)
+    // console.log("radius1: " + radius1)
+    // console.log("radius2: " + radius2)
 
     let lineOfDistance = Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)))
-    let sumOfRadii = (radius1 + radius2)
+    let sumOfRadii     = (radius1 + radius2)
     // console.log("line of distance: " + lineOfDistance)
     // console.log("radius " + sumOfRadii)
     console.log(lineOfDistance <= sumOfRadii)
@@ -263,86 +271,68 @@ function asteroids() {
     return e.bulletArray.filter((bulletArray) => (
       Number(bulletArray.attr("cx")) <= 600 && (Number(bulletArray.attr("cy")) <= 600) && (Number(bulletArray.attr("cy")) >= 0) && Number(bulletArray.attr("cx")) >= 0
     ))
-  }).subscribe(array => {
+  }).subscribe(array => (
     bulletsArray = array
     // console.log(bulletsArray)
-  })
+  ))
 
-  // logic for asteroid checking for collision with the bullets
+
+  /* LOGIC FOR ASTEROID CHECKING FOR COLLISION WITH THE BULLETS 
+    This works by using the fact that the distance between two points (the line distance) must not be greater than the sum of the two radii.
+    If the line distance is smaller or equal to the two radii combined, then it must have collided. 
+  // */
+  // gameObservable.map(x => ({
+  //   x,
+  //   myBulletArray  : bulletsArray
+  //   myAsteroidArray: asteroidArray
+  // })).map((e) => {
+  //   console.log(myBulletArray)
+  //   console.log(myAsteroidArray)
+    
+  // // let asteroidXCircle = Number(e.currAsteroid.attr("cx"))
+  // // let asteroidYCircle = Number(e.currAsteroid.attr("cy"))
+  // // let asteroidRadius = Number(e.currAsteroid.attr("r"))
+
+  // return e.bulletArray.filter((bullet) => (
+  //   checkCollision(Number(bullet.attr("cx")), asteroidXCircle, Number(bullet.attr("cy")), asteroidYCircle, asteroidRadius, Number(bullet.attr("r")))
+  // ))})
+  // .subscribe((returnArray) => {
+  //   returnArray.forEach((element) => element.elem.remove())
+  //   console.log(returnArray)
+  // })
+
   gameObservable.map(x => ({
     x,
-    currAsteroid: asteroid,
-    bulletArray  : bulletsArray
-  })).map((e) => {
-    
-  let asteroidXCircle = Number(e.currAsteroid.attr("cx"))
-  let asteroidYCircle = Number(e.currAsteroid.attr("cy"))
-  let asteroidRadius = Number(e.currAsteroid.attr("r"))
-
-  return e.bulletArray.filter((bullet) => (
-    checkCollision(Number(bullet.attr("cx")), asteroidXCircle, Number(bullet.attr("cy")), asteroidYCircle, asteroidRadius, Number(bullet.attr("r")))
-  ))})
-  .subscribe((returnArray) => {
-    returnArray.forEach((element) => element.elem.remove())
+    myBulletArray  : bulletsArray,
+    myAsteroidArray: asteroidArray
+  })).map(({x, myBulletArray, myAsteroidArray}) => (
+      myAsteroidArray.filter((asteroid) => (
+      myBulletArray.filter((bullet) => (
+        checkCollision(Number(bullet.attr("cx")), Number(asteroid.attr("cx")), Number(bullet.attr("cy")), Number(asteroid.attr("cy")), Number(asteroid.attr("r")), Number(bullet.attr("r")))
+      ))
+      .map(bullet => (
+        bullet.elem.remove()
+    ))))
+  ))
+  .subscribe((returnArray) => (
     console.log(returnArray)
-    // asteroid2.elem.remove()
-  })
-
-
-  //     return e.bulletArray.forEach((bullet) => {
-//       let asteroidXCircle = Number(e.currAsteroid.attr("cx"))
-//       let asteroidYCircle = Number(e.currAsteroid.attr("cy"))
-//       let asteroidRadius = Number(e.currAsteroid.attr("r"))
-//       let bulletXCircle = Number(bullet.attr("cx"))
-//       let bulletYCircle = Number(bullet.attr("cy"))
-//       let bulletRadius = Number(bullet.attr("r"))
-
-//       return Math.sqrt( Math.pow((asteroidXCircle - bulletXCircle), 2) + Math.pow((asteroidYCircle - bulletYCircle), 2))
-//     })
-//   }).subscribe((bullet) => console.log(bullet))
-// }
-
-
+    // returnArray.forEach((item) => item.elem.remove())
+  ))
 }
 
 
-//   gameObservable.map(x => ({
-//     x,
-//     currAsteroid: asteroid,
-//     bulletArray  : bulletsArray
-//   })).map((e) => {
 
-//   let asteroidXCircle = Number(e.currAsteroid.attr("cx"))
-//   let asteroidYCircle = Number(e.currAsteroid.attr("cy"))
-//   let asteroidRadius = Number(e.currAsteroid.attr("r"))
 
-//   return e.bulletArray.forEach((bullet),
-    
-//   })
-// }).subscribe((e) => ({
-//   currAsteroid.elem.remove()
-// }))
-// let bulletXCircle = Number(bullet.attr("cx"))
-// let bulletYCircle = Number(bullet.attr("cy"))
-// let bulletRadius = Number(bullet.attr("r"))
+if (typeof window != 'undefined')
+  window.onload = () => {
+    asteroids();
+  }
 
-//     return e.bulletArray.forEach((bullet) => {
-//       let asteroidXCircle = Number(e.currAsteroid.attr("cx"))
-//       let asteroidYCircle = Number(e.currAsteroid.attr("cy"))
-//       let asteroidRadius = Number(e.currAsteroid.attr("r"))
-//       let bulletXCircle = Number(bullet.attr("cx"))
-//       let bulletYCircle = Number(bullet.attr("cy"))
-//       let bulletRadius = Number(bullet.attr("r"))
 
-//       return Math.sqrt( Math.pow((asteroidXCircle - bulletXCircle), 2) + Math.pow((asteroidYCircle - bulletYCircle), 2))
-//     })
-//   }).subscribe((bullet) => console.log(bullet))
-// }
+  /* 
+  RANDOM NOTES: 
 
-  // distance of two things and add their radii together and compare 
-  // if that line distance is smaller or equal to othe radius of the two things added toogehter htan it will collide
-
-  // one interval that updates every 10 milliseconods, controols whole game
+  // one interval that updates every 10 milliseconsconsonods, controols whole game
 
 
   // array of 1. It's pure and if you add 2 to it it is still pure as long as long as you work with 1 and not set
@@ -355,8 +345,11 @@ function asteroids() {
   // getBBbox gives it to you in svg cooridnates. That giives yoouo a swuare, then you can do the same thing on your asteroid.
   // Then its the code from oone of the lectures - two squares overlapping. 
 // the following simply runs your asteroids function on window load.  Make sure to leave it in place.
-if (typeof window != 'undefined')
-  window.onload = () => {
-    asteroids();
-  }
+
+  // reduce observables into an array  -> takes emissions from observable and reduces them too initial value
+  // keep time and user input int oobservables and do merge etc.. combine etc... 
+  // there is merge nad merge but merge withh the latest bit 
+  // reduce user input into a game state
+
+  */
 
